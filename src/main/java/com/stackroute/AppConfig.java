cbe7c7d82e9542e771b5a3049d126b7cf19b2c9e
sansreessr <sreenathsankarssr@gmail.com>
2019-09-24 16:35:39 +0530

    task 6

--- src/main/java/com/stackroute/AppConfig.java
+++ src/main/java/com/stackroute/AppConfig.java
@@ -1,8 +1,10 @@
 package com.stackroute;
 
+import com.stackroute.demo.BeanPostProcessorDemoBean;
 import com.stackroute.domain.Actor;
 import com.stackroute.demo.BeanLifecycleDemoBean;
 import com.stackroute.domain.Movie;
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
@@ -37,5 +39,10 @@ public class AppConfig {
     @Bean(initMethod = "customInit", destroyMethod = "customDestroy")
     public BeanLifecycleDemoBean lCycle() {
         return new BeanLifecycleDemoBean();
+    }
+
+    @Bean
+    public BeanPostProcessorDemoBean postProcessor () {
+        return new BeanPostProcessorDemoBean();
     }
 }
---
+++ src/main/java/com/stackroute/demo/BeanPostProcessorDemoBean.java
@@ -0,0 +1,19 @@
+package com.stackroute.demo;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+
+public class BeanPostProcessorDemoBean implements BeanPostProcessor {
+
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException{
+        System.out.println("processing bean instance before initialization; after spring instantiate event and before init life-cycle event of " + beanName);
+        return bean;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println("processing bean instance after initialization; just after init life-cycle event of " + beanName);
+        return bean;
+    }
+}
